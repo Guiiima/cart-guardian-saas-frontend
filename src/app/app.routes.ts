46607c0b7b67494498c75cc52960731bb9f65d02
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-08-12 21:14:25 -0300

    ++

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { LoginScreen } from './features/login-screen/login-screen';
-import { Setting } from './features/setting/setting';
+import { Setting } from './components/setting/setting';
 
 export const routes: Routes = [
    { path: '', component: Setting, title: 'Login' },
