4ca1ac90588da47a76c80b7f89eb41f68b37b275
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-08-12 21:12:40 -0300

    ++

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -24,6 +24,7 @@ import { MatError } from '@angular/material/form-field';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    
   ],
 })
 export class Login {
--- src/app/features/setting/setting.ts
+++ src/app/features/setting/setting.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -23,43 +24,43 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './setting.html',
   styleUrls: ['./setting.scss']
 })
-export class Setting {
+export class Setting implements OnInit {
   settingsForm!: FormGroup;
 
-  // constructor(
-  //   private fb: FormBuilder,
-  //   private dataService: DataService
-  //   // private snackBar: MatSnackBar
-  // ) { }
+  constructor(
+    private fb: FormBuilder,
+    //private dataService: DataService,
+    private snackBar: MatSnackBar
+  ) { }
 
-  // ngOnInit(): void {
-  //   this.settingsForm = this.fb.group({
-  //     appName: [''],
-  //     enableNotifications: [false],
-  //     theme: ['light']
-  //   });
+  ngOnInit(): void {
+  this.settingsForm = this.fb.group({
+    notificationMessage: ['', Validators.required], 
+    enableNotifications: [true],                    
+    sendDelay: [10, [Validators.required, Validators.min(1)]] 
+  });
 
-  //   this.loadSettings();
-  // }
+    this.loadSettings();
+  }
 
-  // loadSettings(): void {
-  //   this.dataService.getSettings().subscribe((settings: { [key: string]: any; }) => {
-  //     this.settingsForm.patchValue(settings);
-  //   });
-  // }
+  loadSettings(): void {
+    // this.dataService.getSettings().subscribe((settings: { [key: string]: any; }) => {
+    //   this.settingsForm.patchValue(settings);
+    // });
+  }
 
-  // onSave(): void {
-  //   if (this.settingsForm.valid && this.settingsForm.dirty) {
-  //     this.dataService.saveSettings(this.settingsForm.value).subscribe({
-  //       next: () => {
-  //         // this.snackBar.open('Configurações salvas com sucesso!', 'Fechar', { duration: 3000 });
-  //         this.settingsForm.markAsPristine();
-  //       },
-  //       error: (err: any) => console.error('Erro ao salvar', err) // Adicionar notificação de erro
-  //     });
-  //   }
-  // }
   onSave(): void {
-
+    // if (this.settingsForm.valid && this.settingsForm.dirty) {
+    //   this.dataService.saveSettings(this.settingsForm.value).subscribe({
+    //     next: () => {
+    //       this.snackBar.open('Configurações salvas com sucesso!', 'Fechar', { duration: 3000 });
+    //       this.settingsForm.markAsPristine();
+    //     },
+    //     error: (err: any) => {
+    //       console.error('Erro ao salvar as configurações', err);
+    //       this.snackBar.open('Erro ao salvar as configurações.', 'Fechar', { duration: 3000 });
+    //     }
+    //   });
+    // }
   }
 }
