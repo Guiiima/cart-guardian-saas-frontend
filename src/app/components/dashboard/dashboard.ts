55c190c82e61b667fc4f769906cbd83793ce58a3
Guilherme Henrique <guilhermeh000@hotmail.com>
2025-08-12 21:55:19 -0300

    css para menu drop down

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
+  isDropdownOpen = false;
+  constructor(private elementRef: ElementRef) { }
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  // 3. (Opcional, mas recomendado) Função para fechar o menu ao clicar fora dele
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    // Verifica se o clique foi fora do container do dropdown e se o menu está aberto
+    if (!this.elementRef.nativeElement.contains(event.target) && this.isDropdownOpen) {
+      this.isDropdownOpen = false;
+    }
+  }
 
 }
