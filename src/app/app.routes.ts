e94e6621a351c9e2d4f354b06bf6f59a1ecab676
Guiiima <guilhermeh000@hotmail.com>
2025-10-02 22:39:25 -0300

    ++

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { Navbar } from './components/navbar/navbar';
 import { TemplateSelector } from '@features/template-selector/template-selector';
 import { Search } from './components/search/search';
 import { PreviewPanel } from './components/preview-panel/preview-panel';
+import { NotificationSettings } from '@features/notification-settings/notification-settings';
 
 export const routes: Routes = [
    { path: '', component: PreviewPanel, title: 'DashBoard' },
    { path: 'HomeSreen', component: HomeScreen, title: 'DashBoard' },
    { path: 'TemplateSelector', component: TemplateSelector, title: 'DashBoard' },
+   { path: 'NotificationSettings', component: NotificationSettings, title: 'NotificationSettings' },
 ];
